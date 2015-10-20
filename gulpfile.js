d5a08a2cd1f4d52c1a42dcfd79b9ae18652efe9c
Jeffrey <jeffreysmithart@gmail.com>
2015-10-19 20:45:03 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify');
+
+gulp.task('styles', function() {
+    return gulp.src('src/scss/app.scss')
+    .pipe(sass({style: 'expanded'}))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('temp'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(gulp.dest('temp'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'))
+});
+
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/scss/*.scss', ['styles']);
+
+  // // Watch .js files
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+  // // Watch image files
+  // gulp.watch('src/images/**/*', ['images']);
+
+});
